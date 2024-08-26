d17282b409d767298fe9866d46a44da7303f1fdf
Alex <fan.hu@myir.cn>
2024-08-26 15:58:08 +0800

    feat: add myt-stm32mp257x
    Signed-off-by: Alex <fan.hu@myir.cn>

--- core/arch/arm/plat-stm32mp2/conf.mk
+++ core/arch/arm/plat-stm32mp2/conf.mk
@@ -2,10 +2,14 @@ flavor_dts_file-257F_DK = stm32mp257f-dk.dts
 flavor_dts_file-257F_EV1 = stm32mp257f-ev1.dts
 flavor_dts_file-257X_MYB2GB = myb-stm32mp257x-2GB.dts
 flavor_dts_file-257X_MYB1GB = myb-stm32mp257x-1GB.dts
+flavor_dts_file-257X_MYT2GB = myt-stm32mp257x-2GB.dts
+flavor_dts_file-257X_MYT1GB = myt-stm32mp257x-1GB.dts
 
 flavorlist-MP25 = $(flavor_dts_file-257F_DK) \
   		  $(flavor_dts_file-257X_MYB2GB) \
   		  $(flavor_dts_file-257X_MYB1GB) \
+  		  $(flavor_dts_file-257X_MYT2GB) \
+  		  $(flavor_dts_file-257X_MYT1GB) \
 		  $(flavor_dts_file-257F_EV1)
 
 # External device tree default path
@@ -89,7 +93,7 @@ $(call force,CFG_STM32_SAES,n)
 $(call force,CFG_STM32_CRYP,n)
 $(call force,CFG_STM32_HASH,n)
 
-ifneq ($(filter $(CFG_EMBED_DTB_SOURCE_FILE),$(flavorlist-1G) $(flavor_dts_file-257X_MYB1GB) ),)
+ifneq ($(filter $(CFG_EMBED_DTB_SOURCE_FILE),$(flavorlist-1G) $(flavor_dts_file-257X_MYB1GB) $(flavor_dts_file-257X_MYT1GB) ),)
 CFG_DRAM_SIZE    ?= 0x40000000
 endif
 
